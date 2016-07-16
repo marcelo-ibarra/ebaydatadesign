c97f88ef113a9a3028a054ab1b78057280f53cca
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-16 15:04:20 -0600

    Added updates to paragraphs to data design project

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Ebay Data Design Assingment</title>
+	<body>
+		<header>
+			<h1>Ebay Data Design</h1>
+			<h2>User Story</h2>
+			<p>as a user, looking for a pair of brown shoes</p>
+			<h2>Persona</h2>
+			<p>popular site where I can do on-line shopping easy and hassle free. </p>
+		</header>
+		<h2>User Case</h2>
+		<main>
+			<p>Customer wants to be able to go to this site and just make any purchase in a simple and accurate manner.
+				This website displays all types of products desired by customers. Depending on how well this page is
+				designed and how user friendly it is; customer would want to suggest it to other potential users.</p>
+		</main>
+	</body>
+</html>
+
