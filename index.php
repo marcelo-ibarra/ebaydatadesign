97e95a79db8cb497ab984085a27b59ab52104c36
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-16 15:18:30 -0600

    continued with updates to paragraphs to data design project

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
 			<h2>User Story</h2>
 			<p>as a user, looking for a pair of brown shoes</p>
 			<h2>Persona</h2>
-			<p>popular site where I can do on-line shopping easy and hassle free. </p>
+			<p>popular site where I can do on-line shopping easy and hassle free. Being able to browse through different products can help me save lots of time and avoid crowds.</p>
 		</header>
 		<h2>User Case</h2>
 		<main>
 			<p>Customer wants to be able to go to this site and just make any purchase in a simple and accurate manner.
 				This website displays all types of products desired by customers. Depending on how well this page is
-				designed and how user friendly it is; customer would want to suggest it to other potential users.</p>
+				designed and how user friendly it is; customer would want to suggest it to other potential users in the future. This action could be made on any tablet, desk top or mobile device for convenience</p>
 		</main>
 	</body>
 </html>
