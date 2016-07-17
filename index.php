5067ccab9750b31c9bc82ea231fd13a7329caf0e
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-17 17:53:00 -0600

    Made corrections to paragraphs to data design project

--- index.php
+++ index.php
@@ -5,17 +5,27 @@
 	<body>
 		<header>
 			<h1>Ebay Data Design</h1>
+			<hr>
 			<h2>User Story</h2>
 			<p>as a user, looking for a pair of brown shoes</p>
 			<h2>Persona</h2>
 			<p>popular site where I can do on-line shopping easy and hassle free. Being able to browse through different products can help me save lots of time and avoid crowds.</p>
 		</header>
 		<h2>User Case</h2>
-		<main>
 			<p>Customer wants to be able to go to this site and just make any purchase in a simple and accurate manner.
-				This website displays all types of products desired by customers. Depending on how well this page is
-				designed and how user friendly it is; customer would want to suggest it to other potential users in the future. This action could be made on any tablet, desk top or mobile device for convenience</p>
-		</main>
+				This website displays all types of products desired by customers.
+				<br>Depending on how well this page is
+				designed and how user friendly it is; customer would want to suggest it to other potential users in the future.
+				<br>This action could be made on any tablet, desk top or mobile device for convenience</p>
+		<h2>Interaction Flow</h2>
+		<ul>
+			<li>User accesses the page</li>
+			<li>determines category of shoes user desires</li>
+			<li>user then clicks on the pair of shoes he wants</li>
+			<li>takes customer to the "buy now" option</li>
+			<li>then takes user to sign in or "register" screen</li>
+			<li>then taken to final steps of transaction</li>
+			</ul>
 	</body>
 </html>
 
