a69cd6cd056da25095789fa6f0d0af8be985750e
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-18 10:19:49 -0600

    Updated and added a link to data design project

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html lang="en">
 	<head>
 		<title>Ebay Data Design Assingment</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
+	</head>
 	<body>
 		<header>
 			<h1>Ebay Data Design</h1>
@@ -9,23 +11,35 @@
 			<h2>User Story</h2>
 			<p>as a user, looking for a pair of brown shoes</p>
 			<h2>Persona</h2>
-			<p>popular site where I can do on-line shopping easy and hassle free. Being able to browse through different products can help me save lots of time and avoid crowds.</p>
+			<p>Mark - likes this site where he can do on-line shopping easy and hassle free.<br> Being able to browse through different products can help me save lots of time and avoid crowds.<br>
+			Age 41</p>
 		</header>
 		<h2>User Case</h2>
-			<p>Customer wants to be able to go to this site and just make any purchase in a simple and accurate manner.
-				This website displays all types of products desired by customers.
+			<p>Mark wants to be able to go to this site and just make any purchase in a simple and accurate manner.
+				This website displays all types of products desired by many customers.
 				<br>Depending on how well this page is
-				designed and how user friendly it is; customer would want to suggest it to other potential users in the future.
+				designed and how user friendly it is; Mark would want to suggest it to other potential users in the future by going to the site.
+				<a href="https://ebay.com" target="_blank">Go to ebay.com</a>
 				<br>This action could be made on any tablet, desk top or mobile device for convenience</p>
 		<h2>Interaction Flow</h2>
 		<ul>
-			<li>User accesses the page</li>
-			<li>determines category of shoes user desires</li>
-			<li>user then clicks on the pair of shoes he wants</li>
-			<li>takes customer to the "buy now" option</li>
-			<li>then takes user to sign in or "register" screen</li>
-			<li>then taken to final steps of transaction</li>
+			<li>Mark accesses the page</li>
+			<li>He also determines category of shoes he desires</li>
+			<li>Mark then clicks on the pair of shoes he liked</li>
+			<li>site takes Mark to the "buy now" option</li>
+			<li>then it takes Mark to sign in or "register" screen</li>
+			<li>then taken to final steps of transaction - check out</li>
 			</ul>
+		<h2>Conceptual Model</h2>
+		<ol>
+			<li>Every product has a description</li>
+			<li>Each product has a product ID</li>
+			<li>All products have a name</li>
+			<li>There are different categories for all products</li>
+			<li>They all include price</li>
+			<li>Product indicates stock status</li>
+		</ol>
+		<h2>Entitiy Realtionship Diagram</h2>
 	</body>
 </html>
 
