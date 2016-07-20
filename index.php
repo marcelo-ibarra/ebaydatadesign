1877d259b9da99bbdf6507d081fd9efdbab851fa
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-20 09:58:20 -0600

    added updates to data design project

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<title>Ebay Data Design Assingment</title>
-		<link href="css/style.css" rel="stylesheet" type="text/css" />
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
 	</head>
 	<body>
 		<header>
@@ -11,16 +11,18 @@
 			<h2>User Story</h2>
 			<p>as a user, looking for a pair of brown shoes</p>
 			<h2>Persona</h2>
-			<p>Mark - likes this site where he can do on-line shopping easy and hassle free.<br> Being able to browse through different products can help me save lots of time and avoid crowds.<br>
-			Age 41</p>
+			<p>Mark - likes this site where he can do on-line shopping easy and hassle free.<br> Being able to browse
+				through different products can help him save lots of time and avoid crowds.<br>
+				Age 41</p>
 		</header>
 		<h2>User Case</h2>
-			<p>Mark wants to be able to go to this site and just make any purchase in a simple and accurate manner.
-				This website displays all types of products desired by many customers.
-				<br>Depending on how well this page is
-				designed and how user friendly it is; Mark would want to suggest it to other potential users in the future by going to the site.
-				<a href="https://ebay.com" target="_blank">Go to ebay.com</a>
-				<br>This action could be made on any tablet, desk top or mobile device for convenience</p>
+		<p>Mark wants to be able to go to this site and just make any purchase in a simple and accurate manner.
+			This website displays all types of products desired by many customers.
+			<br>Depending on how well this page is
+			designed and how user friendly it is; Mark would want to suggest it to other potential users in the future by
+			going to the site.
+			<a href="https://ebay.com" target="_blank">ebay.com</a>
+			<br>This action could be made on any tablet, desk top or mobile device for convenience</p>
 		<h2>Interaction Flow</h2>
 		<ul>
 			<li>Mark accesses the page</li>
@@ -29,7 +31,7 @@
 			<li>site takes Mark to the "buy now" option</li>
 			<li>then it takes Mark to sign in or "register" screen</li>
 			<li>then taken to final steps of transaction - check out</li>
-			</ul>
+		</ul>
 		<h2>Conceptual Model</h2>
 		<ol>
 			<li>Every product has a description</li>
@@ -39,7 +41,7 @@
 			<li>They all include price</li>
 			<li>Product indicates stock status</li>
 		</ol>
-		<h2>Entitiy Realtionship Diagram</h2>
+		<h2>Entitiy Relationship Diagram</h2>
 	</body>
 </html>
 
